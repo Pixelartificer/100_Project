8a265945c33003e1a441883cfa8f8c70fd3c895d
Lawrence Ranjan D'Cruze <pixelartificer2306@gmail.com>
2025-02-12 16:31:41 +0600

    Add files via upload

---
+++ Project20/JS/Main.js
@@ -0,0 +1,4 @@
+document.getElementById('search-btn').addEventListener('click', function() {
+    let query = document.getElementById('search-bar').value;
+    alert('Searching for: ' + query);
+});
